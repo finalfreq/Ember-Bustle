ffd422e958716e212fc4d4162fc9a6702460e23f
James Williams & Toby Alden <student@epicodus.com>
2015-06-08 15:38:49 -0700

    Add image carousel.

--- controllers/home.js
+++ controllers/home.js
@@ -1,17 +1,23 @@
 BustleClone.HomeController = Ember.Controller.extend({
-  imgArray: ['http://cdnvideo.dolimg.com/cdn_assets/66c1894e8fb5b98cb722e735712a0ee8c241e23d.jpg', 'http://i.imgur.com/8Pwm8dK.jpg', 'http://imgur.com/M1pZrAX'],
+  imgArray: ['http://cdnvideo.dolimg.com/cdn_assets/66c1894e8fb5b98cb722e735712a0ee8c241e23d.jpg', 'http://i.imgur.com/M1pZrAX.png', 'https://thedullwoodexperiment.files.wordpress.com/2014/02/air-bud-spikes-back-scene.jpg' ],
   index: 1,
-  imgSource: "http://i.imgur.com/8Pwm8dK.jpg",
+  imgSource: 'http://i.imgur.com/M1pZrAX.png',
   actions: {
     leftArrow: function() {
-      var i = this.get('index');
-      var img = this.get('imgArray')[i-1];
-      console.log(i);
-      console.log(img);
-      this.set('imgSource', img);
-      // this.set('imgSource', this.get('imgArray[this.get('index') - 1]'));
-      // this.set('index', this.get('index') - 1);
-      // this.set('imgsource', this.get('imgArray[' + this.get("index") + ']'));
+      if (this.get('index') === 0) {
+        this.set('index', this.get('imgArray').length)
+      }
+      this.set('index', this.get('index') - 1);
+      this.set('imgSource', this.get('imgArray')[this.get('index')]);
+      console.log(this.get('index'));
+    },
+    rightArrow: function() {
+      this.set('index', this.get('index') + 1);
+      if (this.get('index') === this.get('imgArray').length) {
+        this.set('index', 0)
+      }
+      this.set('imgSource', this.get('imgArray')[this.get('index')]);
+      console.log(this.get('index'));
     }
   }
 });
